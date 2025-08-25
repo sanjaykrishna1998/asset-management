68ec26b47ecd65a93797545a66f7fe831365feec
sanjaykrishna1998 <sanjaykrishna1998@gmail.com>
2025-08-25 18:03:10 +0530

    Add files via upload

--- frontend/service-worker.js
+++ frontend/service-worker.js
@@ -3,16 +3,19 @@ const FILES_TO_CACHE = [
   "/",
   "/asset-form.html",
   "/asset-table.html",
-  "/style.css",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png",
   "/manifest.json"
 ];
 
 self.addEventListener("install", (e) => {
+console.log("SW: Install");
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (e) => {
+console.log("SW: Activate");
   e.waitUntil(
     caches.keys().then(keys => Promise.all(keys.map(k => (k !== CACHE_NAME ? caches.delete(k) : null))))
   );
@@ -20,6 +23,15 @@ self.addEventListener("activate", (e) => {
 });
 
 self.addEventListener("fetch", (e) => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  let reqUrl = new URL(e.request.url);
+
+  // Remove query params (Safari adds ?standalone=1)
+  let cacheKey = reqUrl.pathname;
+
+  e.respondWith(
+    caches.match(cacheKey).then((resp) => {
+      return resp || fetch(e.request).catch(() => caches.match("/asset-form.html"));
+    })
+  );
 });
 
