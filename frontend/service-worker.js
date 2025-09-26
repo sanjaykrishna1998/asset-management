b368edda1d4c3a8eabcc2e63a20aaea3483b51ad
sanjaykrishna1998 <sanjaykrishna1998@gmail.com>
2025-09-26 12:48:29 +0530

    Update service-worker.js

--- frontend/service-worker.js
+++ frontend/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "my-pwa-cache-v2";
+const CACHE_NAME = "my-pwa-cache-v4";
 const FILES_TO_CACHE = [
   "/",
   "/asset-form.html",
@@ -71,4 +71,5 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
 
